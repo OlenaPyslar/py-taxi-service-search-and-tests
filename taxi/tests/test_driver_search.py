8f603966966479ae3a1c50deabb149783e45aaf8
Olena Pyslar <pislar33@gmail.com>
2026-03-27 23:55:07 +0200

    Solution Fixed2

--- taxi/tests/test_driver_search.py
+++ taxi/tests/test_driver_search.py
@@ -14,6 +14,7 @@ class DriverSearchTests(TestCase):
         User.objects.create_user(username="john", password="pass", license_number="TER12345")
         User.objects.create_user(username="jane", password="pass", license_number="TST34567")
         User.objects.create_user(username="doe", password="pass", license_number="TST23456")
+        User.objects.create_user(username="jason", password="pass", license_number="TSN34567")
 
     def test_search_by_username_returns_matching_users(self):
         response = self.client.get(reverse("taxi:driver-list") + "?username=ja")
@@ -23,3 +24,23 @@ class DriverSearchTests(TestCase):
         self.assertEqual(len(object_list), 1)
         usernames = [u.username for u in object_list]
         self.assertEqual(set(usernames), ["jane"])
+
+    def test_search_by_username_case_insensitive(self):
+        response = self.client.get(reverse("taxi:driver-list") + "?username=JA")
+
+        self.assertEqual(response.status_code, 200)
+        object_list = response.context["object_list"]
+        self.assertEqual(len(object_list), 1)
+        usernames = [u.username for u in object_list]
+        self.assertEqual(set(usernames), {"jane"})
+
+    def test_search_by_username_multiple_matches(self):
+        response = self.client.get(reverse("taxi:driver-list") + "?username=ja")
+
+        self.assertEqual(response.status_code, 200)
+        object_list = response.context["object_list"]
+        self.assertEqual(len(object_list), 2)
+        usernames = [u.username for u in object_list]
+        self.assertEqual(set(usernames), {"jane", "jason"})
+
+
